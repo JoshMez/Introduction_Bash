1bb5994c897f1d784a6bdd07ec4c1f2dd49e1b6b
JoshMez <joshuamezieres10@gmail.com>
2020-09-21 01:18:42 -0400

    first commit

---
+++ Introduction_Bash_Script/Programming_Flow/C_Style_Loop/multiple_C_Varaible.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# 
+#Aim: To assgin and use multiple variables in C-Style loops. 
+# 
+#C-Style loops, allows for multiple value of iterations. 
+# 
+#example  
+#  
+#Notice how every variable is seperared by commas. 
+# 
+#Then to perform the next action, need to 
+for (( a = 20, b = 6; a > 10; a-- , b++ )); do  
+	echo "$a - $b = $(($a - $b))"
+done 
+
+
+
+exit 0
