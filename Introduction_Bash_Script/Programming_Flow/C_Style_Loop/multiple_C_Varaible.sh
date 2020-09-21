f69b851457915735ff07843fcfc9f24b9acfa6a0
Joshua <65163785+JoshMez@users.noreply.github.com>
2020-09-21 13:22:25 +0800

    Update multiple_C_Varaible.sh

--- Introduction_Bash_Script/Programming_Flow/C_Style_Loop/multiple_C_Varaible.sh
+++ Introduction_Bash_Script/Programming_Flow/C_Style_Loop/multiple_C_Varaible.sh
@@ -8,7 +8,7 @@
 #  
 #Notice how every variable is seperared by commas. 
 # 
-#Then to perform the next action, need to 
+#Then to perform the next action, need to add the semi colons and perform the actions that you wish to take. 
 for (( a = 20, b = 6; a > 10; a-- , b++ )); do  
 	echo "$a - $b = $(($a - $b))"
 done 
